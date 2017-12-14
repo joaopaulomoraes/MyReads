b1807f8e2ef29503cd4d13b1bb38467f11231bf1
João Paulo Moraes <joaopaulomoraes@outlook.com>
2017-12-14 16:48:22 -0200

    feat: Displays the symbolic API categories

--- src/components/books/BooksShelfCategory.js
+++ src/components/books/BooksShelfCategory.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import booksCategory from '../../utils/BooksCategoryAPI'
 
 class BooksShelfCategory extends Component {
   render() {
@@ -6,6 +7,12 @@ class BooksShelfCategory extends Component {
       <div className="book-shelf-changer" id="books-category">
         <select>
           <option value="none" disabled>Move to...</option>
+          {booksCategory.map((shelf) => (
+            <option
+              key={shelf.id}
+              value={shelf.value}
+            >{shelf.title}</option>
+          ))}
           <option value="none">None</option>
         </select>
       </div>
