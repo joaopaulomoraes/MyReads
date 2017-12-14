719c4df842ad3aae5eb06b64279b6bda6d40f2b6
João Paulo Moraes <joaopaulomoraes@outlook.com>
2017-12-14 12:15:00 -0200

    feat: Incorporation of components
    - BooksShelf added
    - Symbolic API "shelfBooks" added to support books and their shelves

--- src/components/MyBooks.js
+++ src/components/MyBooks.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import BooksShelf from './books/BooksShelf'
+import shelfCategory from '../utils/BooksCategoryAPI'
 
 class MyBooks extends Component {
   render() {
     return (
       <div className="list-books-content" id="my-books">
-
+        <BooksShelf shelfCategory={shelfCategory} />
       </div>
     )
   }
