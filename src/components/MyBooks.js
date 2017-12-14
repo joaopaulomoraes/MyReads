ce07b1dfe10e28dfcc85c9445df93225b4e32c6b
João Paulo Moraes <joaopaulomoraes@outlook.com>
2017-12-14 11:55:48 -0200

    feat: MyBooks component added

---
+++ src/components/MyBooks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MyBooks extends Component {
+  render() {
+    return (
+      <div className="list-books-content" id="my-books">
+
+      </div>
+    )
+  }
+}
+
+export default MyBooks
