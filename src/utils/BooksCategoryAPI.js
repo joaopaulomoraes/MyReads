069aa5f2c54036caccd087356420027839f16d84
João Paulo Moraes <joaopaulomoraes@outlook.com>
2017-12-14 10:41:03 -0200

    feat: Simple API to represent BookShelf data
    - Consume this data dynamically
    - If the API provides more shelves the app will automatically support

---
+++ src/utils/BooksCategoryAPI.js
@@ -0,0 +1,19 @@
+const BooksCategoryAPI = [
+  {
+    id: "4UUcueXg",
+    title: "Currently Reading",
+    value: "currentlyReading"
+  },
+  {
+    id: "diAmiBbE",
+    title: "Wanto to Read",
+    value: "wantToRead"
+  },
+  {
+    id: "Qlwn2pWV",
+    title: "Read",
+    value: "read"
+  }
+]
+
+export default BooksCategoryAPI
