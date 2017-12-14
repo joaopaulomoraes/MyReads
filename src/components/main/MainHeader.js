90ecb21249301ddea1ddc3af835a71bd087e0ce9
João Paulo Moraes <joaopaulomoraes@outlook.com>
2017-12-14 11:32:52 -0200

    feat: Main header created

---
+++ src/components/main/MainHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MainHeader extends Component {
+  render() {
+    return (
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+    )
+  }
+}
+
+export default MainHeader
