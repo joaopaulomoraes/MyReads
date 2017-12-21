0f30f74609975eb89bcbc4b373b411a22055dde7
João Paulo Moraes <joaopaulomoraes@outlook.com>
2017-12-21 16:36:12 -0200

    feat: Get the handleBookUpdate function
    - receives the function through props
    - sets the current book value in <select> or if exisiter abritui to "none"

--- src/components/books/BooksShelfCategory.js
+++ src/components/books/BooksShelfCategory.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import booksCategory from '../../utils/BooksCategoryAPI'
+import PropTypes from 'prop-types'
 
 class BooksShelfCategory extends Component {
+  static propType = {
+    book: PropTypes.object.isRequired,
+    handleBookUpdate: PropTypes.func.isRequired
+  }
+
   render() {
+    const { book, handleBookUpdate } = this.props
+
     return (
       <div className="book-shelf-changer" id="books-category">
-        <select>
-          <option value="none" disabled>Move to...</option>
+        <select value={book.shelf ? book.shelf : 'none'} onChange={(e) => handleBookUpdate(book, e.target.value)}>
+          <option disabled>Move to...</option>
           {booksCategory.map((shelf) => (
             <option
               key={shelf.id}
