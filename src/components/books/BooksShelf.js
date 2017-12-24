b5515bf0802247fe3872606fb8a1caf261bbfd1e
João Paulo Moraes <joaopaulomoraes@outlook.com>
2017-12-24 17:56:27 -0200

    refactor: Logic removed from child component
    - Books is received from the parent by props
    - The state is now managed in the parent component

--- src/components/books/BooksShelf.js
+++ src/components/books/BooksShelf.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react'
 import BooksList from './BooksList'
-import * as BooksAPI from '../../utils/BooksAPI'
 import PropTypes from 'prop-types'
 
 class BooksShelf extends Component {
   static propTypes = {
+    books: PropTypes.array.isRequired,
     shelfCategory: PropTypes.array.isRequired,
     handleBookUpdate: PropTypes.func.isRequired
   }
 
-  state = {
-    books: []
-  }
-
-  /**
-   * @description Calls all API books and assigns state
-   * @memberof BooksShelf
-   */
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
-  }
-
   render() {
-    const { shelfCategory, handleBookUpdate } = this.props
-    let { books } = this.state
+    const { books, shelfCategory, handleBookUpdate } = this.props
 
     return (
       <div className="bookshelf">
